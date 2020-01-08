1eef40ae2a58ab59f0fda1977a4b6264e46df9c9
pujasodha <pujasodha@gmail.com>
2020-01-08 15:07:58 -0800

    add cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 stocktwitsRouter = require('./routes/api/stocktwits');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
