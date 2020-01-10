a228d0a6b0d071884b5bc322710e65f181f3b480
pujasodha <pujasodha@gmail.com>
2020-01-10 10:47:52 -0800

    deploy

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ app.post('/api/hello', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/public'))); // Handle React routing, return all requests to React app
+    app.use(express.static(path.join(__dirname, 'client/build'))); // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 
