d72cf238893d75b0943380cc65777a9ae5c0a372
pujasodha <pujasodha@gmail.com>
2020-01-10 11:45:55 -0800

    deploy

--- server/server.js
+++ server/server.js
@@ -22,11 +22,7 @@ app.post('/api/hello', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../client/build'))); // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
+    app.use(express.static('client/build'));
 }
 
 app.use('/stocktwits', stocktwitsRouter);
